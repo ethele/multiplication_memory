c638d3d08ca07c89da2daee466e22d5c194018f7
Ethel <ethele@gmail.com>
2013-09-10 12:01:18 -0700

    Initializing the repository with default app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiplicationMemory::Application.config.secret_key_base = '256dc175d484cd6e4510874827912bfc227737ff13d8f1c0b04c72eb49ed189a5e1d9d1e8b67c0440bf24f90bfef0bafeb35f23ac66c3f457bfc565318288a9d'
